effb0aa599925c01fe9a1114b6aa774a8b21701a
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2021-10-17 13:19:04 -0700

    downloaded 11.4 file

--- routes/apiRoutes/animalRoutes.js
+++ routes/apiRoutes/animalRoutes.js
@@ -1,8 +1,8 @@
+const router = require('express').Router();
 const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
 const { animals } = require('../../data/animals');
-const router = require('express').Router();
 
-app.get('/animals', (req, res) => {
+router.get('/animals', (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
@@ -10,7 +10,7 @@ app.get('/animals', (req, res) => {
   res.json(results);
 });
 
-app.get('/animals/:id', (req, res) => {
+router.get('/animals/:id', (req, res) => {
   const result = findById(req.params.id, animals);
   if (result) {
     res.json(result);
@@ -19,7 +19,7 @@ app.get('/animals/:id', (req, res) => {
   }
 });
 
-app.post('/animals', (req, res) => {
+router.post('/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
@@ -31,4 +31,4 @@ app.post('/animals', (req, res) => {
   }
 });
 
-module.exports  = router;
+module.exports = router;
