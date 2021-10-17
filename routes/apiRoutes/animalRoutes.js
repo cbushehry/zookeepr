fa66a4575ff6dafa242a31c8ea16252e976c3ce1
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2021-10-17 13:14:27 -0700

    refactored code, added jest

--- routes/apiRoutes/animalRoutes.js
+++ routes/apiRoutes/animalRoutes.js
@@ -1,1 +1,34 @@
-test
+const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
+const { animals } = require('../../data/animals');
+const router = require('express').Router();
+
+app.get('/animals', (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+app.get('/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+app.post('/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+
+module.exports  = router;
